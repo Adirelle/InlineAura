986df417a8ccd578029b06564bb1746c95fc976f
adirelle <adirelle@6b8d8e06-dcca-43c1-91c7-1659fbf8aa08>
2010-10-18 04:22:03 +0000

    Do not load ClassDefaults.lua instead of bailing out at the start of the file.

--- ClassDefaults.lua
+++ ClassDefaults.lua
@@ -21,8 +21,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ------------------------------------------------------------------------------
 if not InlineAura then return end
 
-do return end -- Not default has of now
-
 local addonName, ns = ...
 
 local InlineAura = InlineAura

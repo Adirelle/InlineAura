14303dd92c8338ee382bae1bd5028ddf187ec01f
Adirelle <adirelle@gmail.com>
2012-08-29 08:12:50 +0200

    Updated mail and copyleft notice.

--- Display.lua
+++ Display.lua
@@ -1,6 +1,6 @@
 --[[
 Inline Aura - displays aura information inside action buttons
-Copyright (C) 2009-2011 Adirelle (adirelle@tagada-team.net)
+Copyright (C) 2009-2012 Adirelle (adirelle@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
